3847ed0347159ce7a036d72dbdf38d05f99945a1
LuizSouza01 <luizhenri1991@gmail.com>
2021-12-20 13:59:23 -0300

    Repositories implementados

--- src/main/java/com/luiz/bookstore/domain/Livro.java
+++ src/main/java/com/luiz/bookstore/domain/Livro.java
@@ -1,19 +1,33 @@
 package com.luiz.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Livro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public Livro(Integer id, String titulo, String nomeAutor, String texto, Categoria categoria) {
