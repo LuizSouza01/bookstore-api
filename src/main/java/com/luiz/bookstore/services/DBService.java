386209f7a99391cb91fececc75472f306c089e75
LuizSouza01 <luizhenri1991@gmail.com>
2021-12-22 17:15:08 -0300

    Configuração MySQL

--- src/main/java/com/luiz/bookstore/services/DBService.java
+++ src/main/java/com/luiz/bookstore/services/DBService.java
@@ -19,14 +19,22 @@ public class DBService {
 	private LivroRepository livroRepository;
 
 	public void instanciaBaseDados() {
+		
 		Categoria cat1 = new Categoria(null, "Informatica", "Livros de T.I");
-
+		Categoria cat2 = new Categoria(null, "Ficção Cientifica", "Ficção Cientifica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+		
 		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H. G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H. G. Wells", "Lorem ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1, l2));
+		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 
 	}
 }
